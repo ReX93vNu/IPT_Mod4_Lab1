f71b8d5fcf84593c4a0ec23a96ad3720b8c1d100
unknown <sturdevant.david09@gmail.com>
2026-05-23 03:26:12 +0800

    fixed admin page not showing up and added superuser (will the user even show up on git?)

--- mod4_lab1/urls.py
+++ mod4_lab1/urls.py
@@ -25,10 +25,9 @@ router = DefaultRouter()
 router.register(r'student-records', StudentRecordViewSet, basename='student-record')
 
 urlpatterns = [
-    # JWT Auth Endpoints
+    path('admin/', admin.site.urls),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # API Endpoints
     path('api/', include(router.urls)),
 ]
